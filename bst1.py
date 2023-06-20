a5903a923da5df32258db684f373f7b18ee0fa7a
bnaishwaryagowda <137185544+bnaishwaryagowda@users.noreply.github.com>
2023-06-20 20:19:39 +0530

    Add files via upload

---
+++ bst1.py
@@ -0,0 +1,42 @@
+class BST:
+    def __init__(self,key):
+        self.key=key
+        self.lchild=None
+        self.rchild=None
+
+    def insert (self,data):
+        if self.key is None:
+            self.key=data
+            return
+        if self.key>data:
+            if self.lchild:
+                self.lchild.insert(data)
+            else:
+                    self.lchild=BST(data)
+        else:
+                if self.rchild:
+                    self.rchild.insert(data)
+
+                else:
+                    self.lchild=BST(data)
+
+    def search(self,data):
+        if self.key == data:
+            print("Node is found!")
+            return
+        if data < self.key:
+            if self.lchild:
+                self.lchild.search(data)
+            else:
+                print("Node is not present in tree!")
+        else:
+            if self.rchild:
+                self.rchild.search(data)
+            else:
+                print("Node is not present in tree!")
+                
+root=BST(10)
+list1=(20,40,30,4,1.5,6)
+for i in list1:
+    root.insert(i)
+root.search(1)
